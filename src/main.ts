41e2cd887b80afa08dc7eeba8db4e24b4b2aa427
relliv <epsilonelittr@gmail.com>
2024-11-12 17:18:23 +0300

    Add node ray config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import veProgress from "vue-ellipse-progress";
 import router from "./routes/app.routes";
 
 import "./demos/ipc";
+
+import { Ray, ray } from "node-ray/web";
+
+window.ray = ray;
+
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 
