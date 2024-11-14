2e8cc67096d57437b362e543ab19276af9ea464c
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-11-14 15:47:44 +0300

    Create SplashView screen

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -7,6 +7,7 @@ import DashboardLayout from "@/layouts/DashboardLayout.vue";
 import HomeView from "@/views/HomeView.vue";
 import TimerView from "@/views/TimerView.vue";
 import SettingsView from "@/views/SettingsView.vue";
+import SplashView from "@/views/SplashView.vue";
 
 const routes = [
   {
@@ -27,6 +28,11 @@ const routes = [
         path: "/settings",
         name: "settings",
         component: SettingsView,
+      },
+      {
+        path: "/splash",
+        name: "splash",
+        component: SplashView,
       },
     ],
   },
