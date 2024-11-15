74c5f443bf4f3f287eca1b98a6e13b404170f1a9
relliv <epsilonelittr@gmail.com>
2024-11-15 03:41:00 +0300

    Add custom title bar for all app

---
+++ src/layouts/components/title-bar/TitleBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="title-bar">
+    <!-- Left -->
+    <div class="left">
+      <h1 class="title">Tombik</h1>
+    </div>
+
+    <!-- Right -->
+    <div class="right">
+      <button>X</button>
+    </div>
+  </div>
+</template>
+
+<style>
+@import "./title-bar.scss";
+</style>
