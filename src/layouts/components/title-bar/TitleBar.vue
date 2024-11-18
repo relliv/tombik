5d3c430e77c5c8c4a55bf698a05b40da05c96be8
Ümit Ahmed <135972028+umitahmedd@users.noreply.github.com>
2024-11-18 20:46:39 +0300

    Implement window control buttons in title bar with maximize, minimize, and close functionality

--- src/layouts/components/title-bar/TitleBar.vue
+++ src/layouts/components/title-bar/TitleBar.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import { Maximize, Minimize, Minus, X } from 'lucide-vue-next';
+import { ref, watchEffect } from 'vue';
+
+const maximize = () => (window as any).ipcRenderer.maximizeWindow();
+const minimize = () => (window as any).ipcRenderer.minimizeWindow();
+const close = () => (window as any).ipcRenderer.closeWindow();
+
+const isWindowMaximized = ref(false);
+
+watchEffect(async () => {
+  isWindowMaximized.value = await (window as any).ipcRenderer.isWindowMaximized()
+
+  const unsubscribe = (window as any).ipcRenderer.onMaximizedChange((event: any, maximized: any) => {
+    isWindowMaximized.value = maximized
+  })
+
+  return () => {
+    unsubscribe()
+  }
+})
+</script>
+
 <template>
   <div class="title-bar">
     <!-- Left -->
@@ -7,7 +30,16 @@
 
     <!-- Right -->
     <div class="right">
-      <button>X</button>
+      <button class="action" @click="maximize">
+        <Maximize v-if="!isWindowMaximized" />
+        <Minimize v-else />
+      </button>
+      <button class="action" @click="minimize">
+        <Minus />
+      </button>
+      <button class="action" @click="close">
+        <X />
+      </button>
     </div>
   </div>
 </template>
