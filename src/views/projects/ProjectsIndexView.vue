27e87a5a38cea7c4c2e807bd6c544781b26f8634
Eyüp <epsilonelittr@gmail.com>
2025-03-15 23:29:26 +0300

    Display project folders in table view

--- src/views/projects/ProjectsIndexView.vue
+++ src/views/projects/ProjectsIndexView.vue
@@ -18,11 +18,71 @@
       </DialogContent>
     </Dialog>
 
-    <ul>
-      <li v-for="folder in folders" :key="folder.fullPath">
-        {{ folder.name }}
-      </li>
-    </ul>
+    <div class="rounded-md border">
+      <Table>
+        <TableHeader>
+          <TableRow
+            v-for="headerGroup in table.getHeaderGroups()"
+            :key="headerGroup.id"
+          >
+            <TableHead
+              v-for="header in headerGroup.headers"
+              :key="header.id"
+              :data-pinned="header.column.getIsPinned()"
+              :class="
+                cn(
+                  { 'sticky bg-background/95': header.column.getIsPinned() },
+                  header.column.getIsPinned() === 'left' ? 'left-0' : 'right-0'
+                )
+              "
+            >
+              <FlexRender
+                v-if="!header.isPlaceholder"
+                :render="header.column.columnDef.header"
+                :props="header.getContext()"
+              />
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <template v-if="table.getRowModel().rows?.length">
+            <template v-for="row in table.getRowModel().rows" :key="row.id">
+              <TableRow :data-state="row.getIsSelected() && 'selected'">
+                <TableCell
+                  v-for="cell in row.getVisibleCells()"
+                  :key="cell.id"
+                  :data-pinned="cell.column.getIsPinned()"
+                  :class="
+                    cn(
+                      { 'sticky bg-background/95': cell.column.getIsPinned() },
+                      cell.column.getIsPinned() === 'left'
+                        ? 'left-0'
+                        : 'right-0'
+                    )
+                  "
+                >
+                  <FlexRender
+                    :render="cell.column.columnDef.cell"
+                    :props="cell.getContext()"
+                  />
+                </TableCell>
+              </TableRow>
+              <TableRow v-if="row.getIsExpanded()">
+                <TableCell :colspan="row.getAllCells().length">
+                  {{ JSON.stringify(row.original) }}
+                </TableCell>
+              </TableRow>
+            </template>
+          </template>
+
+          <TableRow v-else>
+            <TableCell :colspan="columns.length" class="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   </div>
 </template>
 
@@ -37,19 +97,146 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import { ref, onMounted } from "vue";
-import { IBasicFolder } from "@/shared/models/file/folder.model.ts";
+import { ref, onMounted, h } from "vue";
+import { IBasicFolder } from "@/shared/models/file/folder.model";
+import type {
+  ColumnFiltersState,
+  ExpandedState,
+  SortingState,
+  VisibilityState,
+} from "@tanstack/vue-table";
+import { cn, valueUpdater } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  createColumnHelper,
+  FlexRender,
+  getCoreRowModel,
+  getExpandedRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useVueTable,
+} from "@tanstack/vue-table";
+import { ChevronDown, ChevronsUpDown } from "lucide-vue-next";
 
 const folders = ref<IBasicFolder[]>([]);
 const projectName = ref("");
 
+const columnHelper = createColumnHelper<IBasicFolder>();
+
+const columns = [
+  columnHelper.display({
+    id: "select",
+    header: ({ table }) =>
+      h(Checkbox, {
+        modelValue:
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate"),
+        "onUpdate:modelValue": (value) =>
+          table.toggleAllPageRowsSelected(!!value),
+        ariaLabel: "Select all",
+      }),
+    cell: ({ row }) => {
+      return h(Checkbox, {
+        modelValue: row.getIsSelected(),
+        "onUpdate:modelValue": (value) => row.toggleSelected(!!value),
+        ariaLabel: "Select row",
+      });
+    },
+    enableSorting: false,
+    enableHiding: false,
+  }),
+  columnHelper.accessor("name", {
+    enablePinning: true,
+    header: "Name",
+    cell: ({ row }) => h("div", { class: "capitalize" }, row.getValue("name")),
+  }),
+  // columnHelper.display({
+  //   id: "actions",
+  //   enableHiding: false,
+  //   cell: ({ row }) => {
+  //     const payment = row.original;
+
+  //     return h(
+  //       "div",
+  //       { class: "relative" },
+  //       h(DropdownAction, {
+  //         payment,
+  //         onExpand: row.toggleExpanded,
+  //       })
+  //     );
+  //   },
+  // }),
+];
+
+const sorting = ref<SortingState>([]);
+const columnFilters = ref<ColumnFiltersState>([]);
+const columnVisibility = ref<VisibilityState>({});
+const rowSelection = ref({});
+const expanded = ref<ExpandedState>({});
+
+const table = useVueTable({
+  data: folders,
+  columns,
+  getCoreRowModel: getCoreRowModel(),
+  getPaginationRowModel: getPaginationRowModel(),
+  getSortedRowModel: getSortedRowModel(),
+  getFilteredRowModel: getFilteredRowModel(),
+  getExpandedRowModel: getExpandedRowModel(),
+  onSortingChange: (updaterOrValue) => valueUpdater(updaterOrValue, sorting),
+  onColumnFiltersChange: (updaterOrValue) =>
+    valueUpdater(updaterOrValue, columnFilters),
+  onColumnVisibilityChange: (updaterOrValue) =>
+    valueUpdater(updaterOrValue, columnVisibility),
+  onRowSelectionChange: (updaterOrValue) =>
+    valueUpdater(updaterOrValue, rowSelection),
+  onExpandedChange: (updaterOrValue) => valueUpdater(updaterOrValue, expanded),
+  state: {
+    get sorting() {
+      return sorting.value;
+    },
+    get columnFilters() {
+      return columnFilters.value;
+    },
+    get columnVisibility() {
+      return columnVisibility.value;
+    },
+    get rowSelection() {
+      return rowSelection.value;
+    },
+    get expanded() {
+      return expanded.value;
+    },
+    columnPinning: {
+      left: ["status"],
+    },
+  },
+});
+
 const loadProjectFolders = async () => {
-  folders.value = await window.ipcRenderer.getWorkspaceFolders();
+  folders.value = await (window as any).ipcRenderer.getWorkspaceFolders();
 };
 
 const createProject = async (projectName: string) => {
   if (projectName) {
-    const success = await window.ipcRenderer.createNewProject(projectName);
+    const success = await (window as any).ipcRenderer.createNewProject(
+      projectName
+    );
 
     if (success) {
       loadProjectFolders();
