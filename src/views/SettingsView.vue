55d30369bba29872cf67eff851b44d30cc7d5932
Eyüp <epsilonelittr@gmail.com>
2025-03-16 00:05:55 +0300

    Set settings page title and description

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -4,4 +4,13 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAppStore } from "@/shared/stores/app.store";
+import { onMounted } from "vue";
+
+const appStore = useAppStore();
+
+onMounted(async () => {
+  appStore.setPageTitle("Settings", "Change app settings");
+});
+</script>
