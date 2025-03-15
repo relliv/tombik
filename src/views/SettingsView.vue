c5d674980ffd4b2c722c42506a3af15abaf411df
Eyüp <epsilonelittr@gmail.com>
2025-03-16 00:08:28 +0300

    Add dummy settings menus

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,14 +1,55 @@
 <template>
-  <div>
-    <h1>Settings</h1>
-  </div>
+  <Accordion
+    type="single"
+    class="w-full"
+    collapsible
+    :default-value="defaultValue"
+  >
+    <AccordionItem
+      v-for="item in accordionItems"
+      :key="item.value"
+      :value="item.value"
+    >
+      <AccordionTrigger>{{ item.title }}</AccordionTrigger>
+      <AccordionContent>
+        {{ item.content }}
+      </AccordionContent>
+    </AccordionItem>
+  </Accordion>
 </template>
 
 <script setup lang="ts">
-import { useAppStore } from "@/shared/stores/app.store";
 import { onMounted } from "vue";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { useAppStore } from "@/shared/stores/app.store";
 
 const appStore = useAppStore();
+
+const defaultValue = "item-1";
+
+const accordionItems = [
+  {
+    value: "item-1",
+    title: "Is it accessible?",
+    content: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    value: "item-2",
+    title: "Is it unstyled?",
+    content:
+      "Yes. It's unstyled by default, giving you freedom over the look and feel.",
+  },
+  {
+    value: "item-3",
+    title: "Can it be animated?",
+    content: "Yes! You can use the transition prop to configure the animation.",
+  },
+];
 
 onMounted(async () => {
   appStore.setPageTitle("Settings", "Change app settings");
