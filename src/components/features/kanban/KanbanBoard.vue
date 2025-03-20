5828cf1d4008065f265df341e4eefd615e95dcf5
Eyüp <epsilonelittr@gmail.com>
2025-03-21 00:24:20 +0300

    Update scroll style of kanban view

--- src/components/features/kanban/KanbanBoard.vue
+++ src/components/features/kanban/KanbanBoard.vue
@@ -1,118 +1,123 @@
 <template>
-  <div data-vaul-drawer-wrapper="">
-    <div class="kanban-container custom-scrollbar-container">
-      <Container
-        orientation="horizontal"
-        @drop="onColumnDrop($event)"
-        drag-handle-selector=".column-drag-handle"
-        @drag-start="dragStart"
-        :drop-placeholder="upperDropPlaceholderOptions"
-        class="kanban custom-scrollbar"
-      >
-        <Draggable
-          v-for="column in scene.columns"
-          :key="column.id"
-          class="column custom-scrollbar-container"
-        >
-          <!-- Column Header -->
-          <div class="header">
-            <div class="left">
-              <span class="title">
-                {{ column.title }}
-              </span>
-
-              <span class="count">
-                {{ column.tasks.length }}
-              </span>
-            </div>
-
-            <div class="right">
-              <!-- Add New Task -->
-              <button @click="addNewTask(column.id)">
-                <Plus :size="24" />
-              </button>
-            </div>
-          </div>
-
-          <!-- Column Content -->
-          <Container
-            group-name="col"
-            @drop="(e: any) => onCardDrop(column.id, e)"
-            @drag-start="(e: any) => log('drag start', e)"
-            @drag-end="(e: any) => log('drag end', e)"
-            :get-child-payload="getCardPayload(column.id)"
-            drag-class="task-ghost"
-            drop-class="card-ghost-drop"
-            :drop-placeholder="dropPlaceholderOptions"
-            class="task-list custom-scrollbar"
-          >
-            <!-- Draggable Task Items -->
-            <Draggable v-for="card in column.tasks" :key="card.id">
-              <div class="task" @click="onTaskClick(card)">
-                <div class="title">
-                  <div class="checkbox-container">
-                    <Check v-if="card.isDone" :size="12" />
-                    <input
-                      type="checkbox"
-                      v-model="card.isDone"
-                      @click="onTaskStatusChange($event, card)"
-                    />
-                  </div>
-
-                  <h1>{{ card.title }}</h1>
-                </div>
-              </div>
-            </Draggable>
-          </Container>
-        </Draggable>
-      </Container>
-    </div>
-
-    <!-- Task Details Drawer -->
-    <DrawerRoot
-      v-model:open="isTaskDetailsDrawerOpen"
-      :direction="taskDetailsDrawerDirection"
+  <div class="custom-scrollbar-container">
+    <div
+      data-vaul-drawer-wrapper=""
+      class="custom-scrollbar-container custom-scrollbar pb-3"
     >
-      <DrawerPortal>
-        <DrawerOverlay
-          data-testid="overlay"
-          class="fixed inset-0 bg-black/40 w-[500px]"
-        />
-        <DrawerContent
-          data-testid="content"
-          class="bg-zinc-100 flex flex-col rounded-t-[10px] w-[50%] h-full left-[50%] mt-24 fixed bottom-0 right-0"
+      <div class="kanban-container">
+        <Container
+          orientation="horizontal"
+          @drop="onColumnDrop($event)"
+          drag-handle-selector=".column-drag-handle"
+          @drag-start="dragStart"
+          :drop-placeholder="upperDropPlaceholderOptions"
+          class="kanban custom-scrollbar"
         >
-          <div class="p-4 bg-white rounded-t-[10px] flex-1">
-            <div
-              class="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8"
-            />
-            <div class="max-w-md mx-auto">
-              <DrawerTitle class="font-medium mb-4">
-                Unstyled drawer for Vue.
-              </DrawerTitle>
+          <Draggable
+            v-for="column in scene.columns"
+            :key="column.id"
+            class="column custom-scrollbar-container"
+          >
+            <!-- Column Header -->
+            <div class="header">
+              <div class="left">
+                <span class="title">
+                  {{ column.title }}
+                </span>
+
+                <span class="count">
+                  {{ column.tasks.length }}
+                </span>
+              </div>
+
+              <div class="right">
+                <!-- Add New Task -->
+                <button @click="addNewTask(column.id)">
+                  <Plus :size="24" />
+                </button>
+              </div>
             </div>
 
-            <!-- Task Title -->
-            <h1
-              ref="taskTitleInput"
-              :contenteditable="true"
-              @blur="onTaskTitleBlur"
-              class="text-zinc-600 mb-2 font-semibold text-xl w-full p-2 border border-transparent focus:border-tombik-primary-500 rounded-md"
+            <!-- Column Content -->
+            <Container
+              group-name="col"
+              @drop="(e: any) => onCardDrop(column.id, e)"
+              @drag-start="(e: any) => log('drag start', e)"
+              @drag-end="(e: any) => log('drag end', e)"
+              :get-child-payload="getCardPayload(column.id)"
+              drag-class="task-ghost"
+              drop-class="card-ghost-drop"
+              :drop-placeholder="dropPlaceholderOptions"
+              class="task-list custom-scrollbar"
             >
-              {{ selectedTask!.title }}
-            </h1>
-          </div>
-          <button
-            type="button"
-            data-testid="dismiss-button"
-            class="rounded-md mb-6 w-full bg-gray-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
-            @click="() => (isTaskDetailsDrawerOpen = false)"
+              <!-- Draggable Task Items -->
+              <Draggable v-for="card in column.tasks" :key="card.id">
+                <div class="task" @click="onTaskClick(card)">
+                  <div class="title">
+                    <div class="checkbox-container">
+                      <Check v-if="card.isDone" :size="12" />
+                      <input
+                        type="checkbox"
+                        v-model="card.isDone"
+                        @click="onTaskStatusChange($event, card)"
+                      />
+                    </div>
+
+                    <h1>{{ card.title }}</h1>
+                  </div>
+                </div>
+              </Draggable>
+            </Container>
+          </Draggable>
+        </Container>
+      </div>
+
+      <!-- Task Details Drawer -->
+      <DrawerRoot
+        v-model:open="isTaskDetailsDrawerOpen"
+        :direction="taskDetailsDrawerDirection"
+      >
+        <DrawerPortal>
+          <DrawerOverlay
+            data-testid="overlay"
+            class="fixed inset-0 bg-black/40 w-[500px]"
+          />
+          <DrawerContent
+            data-testid="content"
+            class="bg-zinc-100 flex flex-col rounded-t-[10px] w-[50%] h-full left-[50%] mt-24 fixed bottom-0 right-0"
           >
-            Click to close
-          </button>
-        </DrawerContent>
-      </DrawerPortal>
-    </DrawerRoot>
+            <div class="p-4 bg-white rounded-t-[10px] flex-1">
+              <div
+                class="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8"
+              />
+              <div class="max-w-md mx-auto">
+                <DrawerTitle class="font-medium mb-4">
+                  Unstyled drawer for Vue.
+                </DrawerTitle>
+              </div>
+
+              <!-- Task Title -->
+              <h1
+                ref="taskTitleInput"
+                :contenteditable="true"
+                @blur="onTaskTitleBlur"
+                class="text-zinc-600 mb-2 font-semibold text-xl w-full p-2 border border-transparent focus:border-tombik-primary-500 rounded-md"
+              >
+                {{ selectedTask!.title }}
+              </h1>
+            </div>
+            <button
+              type="button"
+              data-testid="dismiss-button"
+              class="rounded-md mb-6 w-full bg-gray-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+              @click="() => (isTaskDetailsDrawerOpen = false)"
+            >
+              Click to close
+            </button>
+          </DrawerContent>
+        </DrawerPortal>
+      </DrawerRoot>
+    </div>
   </div>
 </template>
 
@@ -272,7 +277,7 @@ onMounted(() => {
 
 <style scoped lang="scss">
 .kanban-container {
-  @apply flex flex-col p-5 pb-0 mr-4 h-full;
+  @apply flex flex-col pb-0 mr-4 h-full w-[calc(100vw-200px)];
 
   .kanban {
     @apply flex flex-row gap-5;
@@ -315,7 +320,7 @@ onMounted(() => {
       }
 
       .task-list {
-        @apply flex flex-col gap-2 pr-3 overflow-y-auto h-[calc(100vh-400px)];
+        @apply flex flex-col gap-2 overflow-y-auto h-[calc(100vh-400px)];
 
         &.task-ghost {
           @apply bg-zinc-400 border-zinc-700;
