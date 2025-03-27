5a4fd306f55599055e832c7005d064cae094c26d
Eyüp <epsilonelittr@gmail.com>
2025-03-27 13:02:14 +0300

    Update primary color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,129 +10,129 @@ export default {
   content: ["./src/**/*.{ts,tsx,vue}"],
 
   theme: {
-  	container: {
-  		center: true,
-  		padding: '2rem',
-  		screens: {
-  			'2xl': '1400px'
-  		}
-  	},
-  	extend: {
-  		colors: {
-  			'tombik-primary': {
-  				'50': '#fcfbea',
-  				'100': '#f8f7c9',
-  				'200': '#f2ed96',
-  				'300': '#eee374',
-  				'400': '#e3cb2c',
-  				'500': '#d3b31f',
-  				'600': '#b68e18',
-  				'700': '#926816',
-  				'800': '#79531a',
-  				'900': '#67451c',
-  				'950': '#3c240c'
-  			},
-  			'tombik-secondary': {
-  				'50': '#eff3fe',
-  				'100': '#e1eafe',
-  				'200': '#c9d6fc',
-  				'300': '#a8bbf9',
-  				'400': '#8596f4',
-  				'500': '#747eee',
-  				'600': '#4b4ce0',
-  				'700': '#3f3dc5',
-  				'800': '#34359f',
-  				'900': '#31347e',
-  				'950': '#1d1e49'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			xl: 'calc(var(--radius) + 4px)',
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--reka-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--reka-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			},
-  			'collapsible-down': {
-  				from: {
-  					height: 0
-  				},
-  				to: {
-  					height: 'var(--radix-collapsible-content-height)'
-  				}
-  			},
-  			'collapsible-up': {
-  				from: {
-  					height: 'var(--radix-collapsible-content-height)'
-  				},
-  				to: {
-  					height: 0
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
-  			'collapsible-down': 'collapsible-down 0.2s ease-in-out',
-  			'collapsible-up': 'collapsible-up 0.2s ease-in-out'
-  		}
-  	}
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        "tombik-primary": {
+          50: "#fcfaf8",
+          100: "#f8f3ed",
+          200: "#f0e8db",
+          300: "#e4d5be",
+          400: "#caab7d",
+          500: "#b68d4e",
+          600: "#775b31",
+          700: "#574223",
+          800: "#332715",
+          900: "#19130a",
+          950: "#0e0b06",
+        },
+        "tombik-secondary": {
+          "50": "#eff3fe",
+          "100": "#e1eafe",
+          "200": "#c9d6fc",
+          "300": "#a8bbf9",
+          "400": "#8596f4",
+          "500": "#747eee",
+          "600": "#4b4ce0",
+          "700": "#3f3dc5",
+          "800": "#34359f",
+          "900": "#31347e",
+          "950": "#1d1e49",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      borderRadius: {
+        xl: "calc(var(--radius) + 4px)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--reka-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--reka-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+        "collapsible-down": {
+          from: {
+            height: 0,
+          },
+          to: {
+            height: "var(--radix-collapsible-content-height)",
+          },
+        },
+        "collapsible-up": {
+          from: {
+            height: "var(--radix-collapsible-content-height)",
+          },
+          to: {
+            height: 0,
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "collapsible-down": "collapsible-down 0.2s ease-in-out",
+        "collapsible-up": "collapsible-up 0.2s ease-in-out",
+      },
+    },
   },
   plugins: [animate, setupInspiraUI, require("tailwindcss-animate")],
 };
