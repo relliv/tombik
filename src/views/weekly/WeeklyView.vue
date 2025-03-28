f80e9f62813ea8e82c8bf0b8ea16f3c79cedc423
Eyüp <epsilonelittr@gmail.com>
2025-03-28 19:23:09 +0300

    Show current week number in hero section

--- src/views/weekly/WeeklyView.vue
+++ src/views/weekly/WeeklyView.vue
@@ -1,14 +1,49 @@
-<template>Testa Weekly View</template>
+<template>
+  <div class="weekly-calendar-view">
+    <div class="hero">
+      <div class="week-number">
+        <span class="label">Week</span>
+        <span class="value">{{ currentWeek }}</span>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, computed } from "vue";
 import { useAppStore } from "@/shared/stores/app.store";
+import { DateTime } from "luxon";
 
 const appStore = useAppStore();
+
+// Calculate current week number using Luxon
+const currentWeek = computed(() => {
+  return DateTime.now().weekNumber;
+});
 
 onMounted(async () => {
   appStore.setPageTitle("Weekly", "Track your weekly to-do lists");
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.weekly-calendar-view {
+  @apply flex flex-col w-full h-full;
+
+  .hero {
+    @apply flex flex-col items-center justify-between;
+
+    .week-number {
+      @apply flex flex-row items-center justify-between text-4xl;
+
+      .label {
+        @apply mr-2 font-medium text-tombik-primary-600;
+      }
+
+      .value {
+        @apply text-8xl font-bold text-tombik-primary-500;
+      }
+    }
+  }
+}
+</style>
