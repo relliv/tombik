9bee85fbe427ce40486f2bfb03460149ccea94d7
Eyüp <epsilonelittr@gmail.com>
2025-03-28 18:42:04 +0300

    Install @kangc/v-md-editor package

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,26 @@ import App from "./App.vue";
 import veProgress from "vue-ellipse-progress";
 import router from "./routes/app.routes";
 
+// #region Markdown Editor
+
+import VueMarkdownEditor from '@kangc/v-md-editor';
+import '@kangc/v-md-editor/lib/style/base-editor.css';
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
+
+import Prism from 'prismjs';
+
+// #endregion
+
 import "./demos/ipc";
 
 import { Ray, ray } from "node-ray/web";
 
 window.ray = ray;
+
+VueMarkdownEditor.use(vuepressTheme, {
+  Prism,
+});
 
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
@@ -19,6 +34,7 @@ const app = createApp(App);
 app.use(router);
 app.use(createPinia());
 app.use(veProgress);
+app.use(VueMarkdownEditor);
 
 app.mount("#app").$nextTick(() => {
   postMessage({ payload: "removeLoading" }, "*");
